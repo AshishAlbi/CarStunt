4e326948ed866ff5a66ffe8c0e13770615e28484
Ashish Albi <ashish.albi@innovaturelabs.com>
2024-05-07 14:38:31 +0530

    update: viteconfig

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,10 @@
 import { defineConfig } from 'vite'
+import { copy } from 'vite-plugin-copy'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    base: "/CarStunt"
+    base: "/CarStunt",
+    plugins: [
+        copy([{ src: 'models', dest: 'dist' }])
+    ]
 })
